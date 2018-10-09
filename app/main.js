75b37fbb81a3466d214d49ca63015490c4206c6c
Romain Racamier-Lafon <romain.racamier@gmail.com>
2018-10-09 16:32:49 +0200

    :tada: structure from nativescript-vue/vue-cli-template
    <3

---
+++ app/main.js
@@ -0,0 +1,15 @@
+import Vue from 'nativescript-vue'
+import App from './components/App'
+import VueDevtools from 'nativescript-vue-devtools'
+
+if(TNS_ENV !== 'production') {
+  Vue.use(VueDevtools)
+}
+// Prints Vue logs when --env.production is *NOT* set while building
+Vue.config.silent = (TNS_ENV === 'production')
+
+Vue.registerElement('RadSideDrawer', () => require('nativescript-ui-sidedrawer').RadSideDrawer)
+
+new Vue({
+  render: h => h('frame', [h(App)])
+}).$start()
