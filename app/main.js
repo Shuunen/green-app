6f8a8f7616211330b9d44c4ebf4387e78bed6ab5
Romain Racamier-Lafon <romain.racamier@gmail.com>
2018-10-10 17:22:44 +0200

    :sparkles: added vue devtools

--- app/main.js
+++ app/main.js
@@ -3,8 +3,9 @@ import App from './components/App'
 import VueDevtools from 'nativescript-vue-devtools'
 
 if(TNS_ENV !== 'production') {
-  Vue.use(VueDevtools)
+  Vue.use(VueDevtools, { host: '192.168.0.24' })
 }
+
 // Prints Vue logs when --env.production is *NOT* set while building
 Vue.config.silent = (TNS_ENV === 'production')
 
