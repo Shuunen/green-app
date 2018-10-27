f481fdc4828a979df10ed2b34e3c5ae2fd083312
Romain Racamier-Lafon <romain.racamier@gmail.com>
2018-10-27 13:55:36 +0200

    just shows products as a guest user

--- app/main.js
+++ app/main.js
@@ -1,11 +1,7 @@
 import Vue from 'nativescript-vue'
 import VueDevtools from 'nativescript-vue-devtools'
-import ApiService from './services/ApiService'
-import Login from './components/Login'
 import Products from './components/Products'
 import store from './store'
-
-const apiService = new ApiService()
 
 if (TNS_ENV !== 'production') {
   Vue.use(VueDevtools, { host: '192.168.0.24' })
@@ -16,7 +12,16 @@ Vue.config.silent = (TNS_ENV === 'production')
 
 Vue.registerElement('RadSideDrawer', () => require('nativescript-ui-sidedrawer').RadSideDrawer)
 
+// Solution 1 : If we want to force login first
+// import ApiService from './services/ApiService'
+// import Login from './components/Login'
+// const apiService = new ApiService()
+// const loginFirst = h => h('frame', [h(apiService.isLoggedIn() ? Products : Login)])
+
+// Solution 2 : just shows products as a guest user
+const guestProducts = h => h('frame', [h(Products)])
+
 new Vue({
-  render: h => h('frame', [h(apiService.isLoggedIn() ? Products : Login)]),
+  render: guestProducts,
   store
 }).$start()
