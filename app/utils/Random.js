9601d972923361fc7b1cac139b72248b532b8bdf
Romain Racamier-Lafon <romain.racamier@gmail.com>
2018-11-03 17:44:53 +0100

    now using shuutils

--- app/utils/Random.js
+++ app/utils/Random.js
@@ -1,40 +1,24 @@
-// specific
+import { pickOne, getRandomNumber, getRandomString } from 'shuutils'
+
 const data = {
   id: 1,
-  types: ['starter', 'main', 'dessert'],
-  names: ['Salad salty', 'Spicy chicken', 'Pooled Fry', 'Jujuba Coconut', 'Embas', 'Cascady fills', 'Fluffs', 'Koppa Cabana']
+  types: ['starter', 'main', 'dessert']
 }
 
 export default class Random {
-  // generic
-  static getString () {
-    return (
-      data.names.pop() ||
-      Math.random()
-        .toString(36)
-        .substring(7)
-    )
-  }
-  static getNumber (min, max) {
-    // min and max included
-    return Math.floor(Math.random() * (max - min + 1) + min)
-  }
-  static pickOne (arr) {
-    return arr[Math.floor(Math.random() * arr.length)]
-  }
   // specific
   static getPriceCents () {
-    return this.pickOne([0, 0.5, 0.45, 0.99, 0.75, 0.80])
+    return pickOne([0, 0.5, 0.45, 0.99, 0.75, 0.80])
   }
   static getPrice () {
-    return this.getNumber(2, 9) + this.getPriceCents()
+    return getRandomNumber(2, 9) + this.getPriceCents()
   }
   static getProduct () {
     return {
       id: data.id++,
-      name: this.getString(),
+      name: getRandomString(),
       price: this.getPrice(),
-      type: this.pickOne(data.types)
+      type: pickOne(data.types)
     }
   }
   static getProducts () {
