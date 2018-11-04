25ef3d0be2221d4043de43e70f648b81535e8550
Romain Racamier-Lafon <romain.racamier@gmail.com>
2018-11-04 11:30:18 +0100

    extracted menu

---
+++ app/components/Menu.vue
@@ -0,0 +1,24 @@
+<template>
+  <StackLayout  backgroundColor="#ffffff">
+    <Label class="drawer-header" text="Drawer"/>
+    <Label class="drawer-item" text="Menus"/>
+    <Label class="drawer-item" text="My account"/>
+    <Label class="drawer-item" text="Logout" @tap="doLogout"/>
+  </StackLayout>
+</template>
+
+<style>
+.drawer-header {
+  padding: 50 16 16 16;
+  margin-bottom: 16;
+  background-color: #53ba82;
+  color: #ffffff;
+  font-size: 24;
+}
+
+.drawer-item {
+  padding: 8 16;
+  color: #333333;
+  font-size: 16;
+}
+</style>
--- app/components/Products.vue
+++ app/components/Products.vue
@@ -4,18 +4,12 @@
             <GridLayout width="100%" columns="auto, *, auto">
                 <Label class="menu" text="MENU" @tap="$refs.drawer.nativeView.showDrawer()" col="0"/>
                 <Label class="title" text="Welcome to Green !" col="1" />
-                <Icon class="cart" name="shopping-cart-white" :number="cartCount" col="2" />
+                <Icon class="cart" name="shopping-cart-white" :number="3" col="2" />
             </GridLayout>
         </ActionBar>
 
         <RadSideDrawer ref="drawer">
-            <StackLayout ~drawerContent backgroundColor="#ffffff">
-                <Label class="drawer-header" text="Drawer"/>
-                <Label class="drawer-item" text="Menus"/>
-                <Label class="drawer-item" text="My account"/>
-                <Label class="drawer-item" text="Logout" @tap="doLogout"/>
-            </StackLayout>
-
+            <Menu ~drawerContent />
             <StackLayout ~mainContent @loaded="load()">
                 <Icon class="app-logo" name="logo" size="210x80" />
                 <label class="app-subtitle" text="Online Order" />
@@ -29,12 +23,14 @@
 <script>
 import store from "../store";
 import Icon from "@/components/Icon";
+import Menu from "@/components/Menu";
 import { mapGetters } from "vuex";
 import { mapActions } from "vuex";
 
 export default {
   components: {
-    Icon
+    Icon,
+    Menu
   },
   data() {
     return {
@@ -108,20 +104,6 @@ ActionBar {
   color: #333333;
   margin-bottom: 10;
   margin-left: 3;
-}
-
-.drawer-header {
-  padding: 50 16 16 16;
-  margin-bottom: 16;
-  background-color: #53ba82;
-  color: #ffffff;
-  font-size: 24;
-}
-
-.drawer-item {
-  padding: 8 16;
-  color: #333333;
-  font-size: 16;
 }
 
 .cart {
