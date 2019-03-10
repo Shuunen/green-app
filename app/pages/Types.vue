6bd8c2bae1aa51246eb30f710c4aeab93723664f
Romain Racamier-Lafon <romain.racamier@gmail.com>
2019-03-10 19:41:42 +0100

    setup routing & refactor pages

---
+++ app/pages/Types.vue
@@ -0,0 +1,73 @@
+<template>
+  <Page actionBarHidden="true">
+    <ScrollView orientation="vertical">
+      <StackLayout class="pb10">
+        <Type
+          v-for="(data, index) in typeList"
+          :key="data.type"
+          :data="data"
+          :large="index === 0"
+          @tap.native="selectType(data)"
+        />
+      </StackLayout>
+    </ScrollView>
+  </Page>
+</template>
+
+<script>
+import Type from "@/components/Type";
+import { mapGetters } from "vuex";
+import { mapActions } from "vuex";
+import TypeDetails from "@/pages/TypeDetails";
+
+export default {
+  components: {
+    Type
+  },
+  data() {
+    return {
+      listLoaded: false
+    };
+  },
+  computed: {
+    ...mapGetters({
+      typeList: "typeList",
+      isLoading: "isLoading"
+    })
+  },
+  created() {
+    console.log("Types created");
+    this.load();
+  },
+  methods: {
+    ...mapActions(["loadTypes"]),
+    load() {
+      this.loadTypes()
+        .catch(error => {
+          console.error(error);
+          alert("An error occurred loading types list.");
+        })
+        .then(() => {
+          this.listLoaded = true;
+          // this.gotoFirstType();
+        });
+    },
+    /*
+    gotoFirstType() {
+      setTimeout(() => console.log("\n\nPLEASE REMOVE ME ^^ "), 1000);
+      this.$navigateTo(TypeDetails, {
+        frame: "mainContent",
+        props: { data: this.typeList[0] }
+      });
+    },
+    */
+    selectType(data) {
+      console.log("user wants to see type :", data);
+      this.$navigateTo(TypeDetails, {
+        frame: "mainContent",
+        props: { data }
+      });
+    }
+  }
+};
+</script>
