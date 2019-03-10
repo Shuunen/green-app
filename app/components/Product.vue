7c530d0b0bbc7ee937e0524b2adcb5de08c1e6d3
Romain Racamier-Lafon <romain.racamier@gmail.com>
2019-03-10 19:42:11 +0100

    listing page with product component

---
+++ app/components/Product.vue
@@ -0,0 +1,84 @@
+<template>
+  <FlexboxLayout
+    class="product h50"
+    :class="[ data.type ]"
+    justifyContent="space-between"
+    alignItems="center"
+  >
+    <Icon class="product-icon w30" flexShrink="0" name="logo-g" size="small"/>
+    <Label class="product-name ml5" flexGrow="1" :text="data.name"/>
+    <Label class="product-price w50 ml10 mr5" flexShrink="0" :text="formatPrice(data.price)"/>
+    <Icon
+      class="product-add w30 mr5"
+      flexShrink="0"
+      name="shopping-cart-add-grey"
+      size="small"
+      @tap.native="addProduct(data)"
+    />
+  </FlexboxLayout>
+</template>
+
+<script>
+import Icon from "@/components/Icon";
+import Formatter from "@/utils/Formatter";
+import { mapGetters } from "vuex";
+import { mapActions } from "vuex";
+
+/* Props data is like :
+{
+  id: 11,
+  name: 'homemade dessert Sin Seguritat',
+  price: 9.45,
+  type: 'dessert'
+}
+*/
+export default {
+  name: "product",
+  props: {
+    data: {
+      type: Object,
+      required: true
+    }
+  },
+  components: {
+    Icon
+  },
+  computed: {
+    ...mapGetters({
+      currency: "currency",
+      locale: "locale",
+      isLoading: "isLoading"
+    })
+  },
+  methods: {
+    formatPrice(num) {
+      return Formatter.price(num, this.locale, this.currency);
+    },
+    addProduct(data) {
+      console.log("user wants to add product :", data.name);
+    }
+  }
+};
+</script>
+
+<style lang="scss">
+@import "../assets/styles";
+
+.product {
+  /* Debug
+  &-icon {
+    background-color: orange;
+  }
+  &-name {
+    background-color: green;
+  }
+  &-price {
+    background-color: purple;
+  }
+  &-add {
+    background-color: palevioletred;
+  }
+  */
+}
+</style>
+
