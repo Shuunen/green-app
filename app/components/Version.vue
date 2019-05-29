d3adc071aef745c32fce5e30a8bbac1a69d78bf2
Romain Racamier-Lafon <romain.racamier@gmail.com>
2019-05-29 14:10:34 +0200

    added version component

---
+++ app/components/Version.vue
@@ -0,0 +1,24 @@
+<template>
+  <FlexboxLayout class="pt5 pb10" justifyContent="center">
+    <Label class="fz10 alt" :text="$t('common.version') + ' ' + pkg.version" />
+  </FlexboxLayout>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+
+import pkg from '../../package.json'
+
+export default {
+  data () {
+    return {
+      pkg
+    }
+  },
+  computed: {
+    ...mapGetters({
+      isLoading: 'isLoading'
+    })
+  }
+}
+</script>
