858d450160bebd17721e632db56f2a824633b2aa
Bertrand Presles <bpresles@gmail.com>
2019-10-01 21:58:13 +0200

    Fixed small lint issue

--- app/services/ConfigService.js
+++ app/services/ConfigService.js
@@ -4,7 +4,7 @@ export default class ConfigService {
 
   loadConfig (configName) {
     let appFolder = knownFolders.currentApp()
-    let cfgFile = appFolder.getFile('config/' + configName +'.json')
+    let cfgFile = appFolder.getFile('config/' + configName + '.json')
 
     return JSON.parse(cfgFile.readTextSync())
   }
