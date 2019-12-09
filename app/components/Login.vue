f9bb5de51f9369eef05df8f552a5fff01ca314df
Romain Racamier-Lafon <romain.racamier@gmail.com>
2019-12-09 19:33:49 +0100

    enhance login screen

--- app/components/Login.vue
+++ app/components/Login.vue
@@ -1,45 +1,65 @@
 <template>
   <Page actionBarHidden="true">
-    <FlexboxLayout flexDirection="column" justifyContent="center" flexGrow="1" class="bg p-l">
-      <SVGImage src="~/assets/images/icons/user.svg" height="200" />
+    <FlexboxLayout flexDirection="column" class="p-l bg">
+      <StackLayout flexGrow="1" />
+      <Icon class="logo mb-l" height="70" name="logo-green-alt" />
+      <StackLayout flexGrow="1">
+        <!--<SVGImage src="~/assets/images/icons/user.svg" height="200" />-->
+        <Label :text="$t('login.welcome')" class="fz-m mt-l mb-l" textWrap="true" />
         <TextField
           v-model="user.email"
-          hint="Email Address"
+          :hint="$t('account.email')"
           keyboardType="email"
           returnKeyType="next"
           :iEnabled="!isAuthenticating"
           autocorrect="false"
           autocapitalizationType="none"
-          row="0"
           @returnPress="focusPassword()"
         />
-      <TextField ref="password" v-model="user.password" hint="Password" secure="true" returnKeyType="done" :isEnabled="!isAuthenticating" row="1" @returnPress="submit()" />
-      <Button class="action mt-m mb-l" :text="isLoggingIn ? $t('account.login') : $t('account.sign-up')" :isEnabled="!isAuthenticating" @tap="submit()" />
+        <TextField ref="password" v-model="user.password" :hint="$t('account.password')" secure="true" returnKeyType="done" :isEnabled="!isAuthenticating" @returnPress="submit()" />
+        <StackLayout class="mt-s" orientation="horizontal">
+          <Switch v-model="hasAccount" />
+          <Label :text="hasAccount ? $t('login.has-account') : $t('login.no-account')" textWrap="true" verticalAlignment="center" class="ml-m" />
+        </StackLayout>
+        <Button class="action validate mt-m" :text="hasAccount ? $t('login.login') : $t('login.sign-up')" :isEnabled="!isAuthenticating" @tap="submit()" />
+        <Label class="mt-s" horizontalAlignment="center" :text="$t('login.forgot-password')" />
+      </StackLayout>
+      <LangSelector />
     </FlexboxLayout>
   </Page>
 </template>
 
 <script>
 import { connectionType, getConnectionType } from 'tns-core-modules/connectivity'
-import Products from '@/components/Products'
+import { mapGetters, mapActions } from 'vuex'
+
+import Logged from '@/pages/Logged'
 import User from '@/models/User'
+import LangSelector from '@/components/LangSelector'
+import Icon from '@/components/Icon'
 
 export default {
-  name: 'Login',
+  components: { LangSelector, Icon },
 
   data () {
     return {
+      hasAccount: true,
       isLoggingIn: true,
       isAuthenticating: false,
       user: new User('romain.racamier@gmail.com', 'my-pass'),
     }
   },
 
+  computed: {
+    ...mapGetters({ isLoading: 'isLoading' }),
+  },
+
   mounted () {
-    console.log('Login mounted')
+    console.log('Login mounted with user', this.user.email)
   },
 
   methods: {
+    ...mapActions(['goAccount', 'startOrder']),
 
     focusPassword () {
       this.$refs.password.nativeView.focus()
@@ -60,10 +80,11 @@ export default {
         alert('app requires an internet connection to log in.')
         return
       }
-      this.isAuthenticating = false
-      console.log('navigating to products page')
-      this.$navigateTo(Products)
-      console.log('navigated to products page')
+      setTimeout(() => {
+        this.isAuthenticating = false
+        console.log('navigating to home page')
+        this.$navigateTo(Logged, { clearHistory: true })
+      }, 4000)
     },
 
   },
@@ -73,11 +94,9 @@ export default {
 <style>
 .main-label {
   horizontal-align: center;
-  color: black;
 }
-.submit-button {
-  background-color: #cb1d00;
-  color: white;
-  margin-top: 15;
+.here {
+  border-width: 2;
+  border-color: salmon;
 }
 </style>
