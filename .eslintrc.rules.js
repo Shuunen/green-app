3fa3f62f021ab0240f2ccf88190c65689a393f3b
Romain Racamier-Lafon <romain.racamier@gmail.com>
2019-12-11 14:29:50 +0100

    add i18n lint

--- .eslintrc.rules.js
+++ .eslintrc.rules.js
@@ -6,5 +6,8 @@ module.exports = {
   'vue/max-attributes-per-line': 'off',
   'vue/singleline-html-element-content-newline': 'off',
   'vue/attribute-hyphenation': 'off',
+  'vue-i18n/no-unused-keys': ['error', {
+    extensions: ['.js', '.vue']
+  }],
   'comma-dangle': ['error', 'always-multiline'],
 }
