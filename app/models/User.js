f0f11133aa009aea9c9eb8fa5b88d706d93a17c7
Romain Racamier-Lafon <romain.racamier@gmail.com>
2019-12-11 13:54:16 +0100

    enhance user class

--- app/models/User.js
+++ app/models/User.js
@@ -1,13 +1,23 @@
 // import validator from 'email-validator'
 
-export default class {
-  constructor (email, password) {
-    this.email = email || ''
-    this.password = password || ''
+export default class User {
+  constructor (data = {}) {
+    this.allergies = data.allergies || []
+    this.diets = data.diets || []
+    this.email = data.email || ''
+    this.password = data.password || ''
+    this.firstName = data.firstName || ''
+    this.lastName = data.lastName || ''
+    this.password = data.password || ''
+    this.store = data.store || ''
   }
 
   isValidEmail () {
     // return validator.validate(this.email)
-    return true
+    return (this.email.length > 0)
+  }
+
+  isValidPassword () {
+    return (this.password.length > 0)
   }
 }
