4e62b4f7c367478a751c871b90740e1dddc1cee6
Romain Racamier-Lafon <romain.racamier@gmail.com>
2019-12-11 13:48:12 +0100

    extracted i18n to a plugin

--- app/main.js
+++ app/main.js
@@ -1,47 +1,26 @@
 import Logged from '@/pages/Logged'
-import Vue from 'nativescript-vue'
-import VueDevtools from 'nativescript-vue-devtools'
+import Login from '@/pages/Login'
+import { i18n } from '@/plugins/i18n'
+import ApiService from '@/services/ApiService'
+import store from '@/store'
 import { SVGImage } from '@teammaestro/nativescript-svg'
-import VueI18n from 'vue-i18n'
-import store from './store'
-import ApiService from './services/ApiService'
-import Login from './components/Login'
+import Vue from 'nativescript-vue'
 
-if (TNS_ENV !== 'production') {
-  // Vue.use(VueDevtools, { host: '192.168.0.24' }) if using a real device
-  Vue.use(VueDevtools)
-}
+// import VueDevtools from 'nativescript-vue-devtools'
+// Vue.use(VueDevtools, { host: '192.168.0.24' }) if using a real device
+// Vue.use(VueDevtools)
 
 Vue.registerElement('SVGImage', () => SVGImage)
 
-/* i18n */
-Vue.use(VueI18n)
-const i18n = new VueI18n({
-  locale: store.state.locale.code,
-  fallbackLocale: store.state.locale.code,
-  messages: {
-    ar: require('~/locales/ar.json'),
-    en: require('~/locales/en.json'),
-    es: require('~/locales/es.json'),
-    fr: require('~/locales/fr.json'),
-  },
-})
-
-// Prints Vue logs when --env.production is *NOT* set while building
-Vue.config.silent = (TNS_ENV === 'production')
+Vue.config.silent = (TNS_ENV === 'production') // Prints Vue logs when --env.production is *NOT* set while building
 
 Vue.registerElement('RadSideDrawer', () => require('nativescript-ui-sidedrawer').RadSideDrawer)
 
-// Solution 1 : If we want to force login first
 const apiService = new ApiService()
 Vue.prototype.$apiService = apiService
-const loginFirst = h => h('frame', [h(apiService.isLoggedIn() ? Logged : Login)])
-
-// Solution 2 : simulate connected user
-// const userLogged = h => h('frame', [h(Logged)])
 
 new Vue({
-  render: loginFirst,
+  render: h => h('frame', [h(apiService.isLoggedIn() ? Logged : Login)]),
   store,
   i18n,
 }).$start()
