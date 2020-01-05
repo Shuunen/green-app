1a8a6e52a01e19803340dfe3cc939e3812d2479d
Romain Racamier-Lafon <romain.racamier@gmail.com>
2020-01-05 16:44:02 +0100

    now signup include store picking

--- app/pages/Signup.vue
+++ app/pages/Signup.vue
@@ -9,15 +9,19 @@
                 <!-- personnal infos -->
                 <Label :text="$t('signup.step-1')" class="fz-m mb-l" textWrap="true" />
                 <StackLayout class="input-field">
-                  <Label :text="$t('account.first-name')" />
+                  <Label :text="$t('account.first-name') + ' *'" />
                   <TextField v-model="user.firstName" />
                 </StackLayout>
                 <StackLayout class="input-field">
                   <Label :text="$t('account.last-name')" />
                   <TextField v-model="user.lastName" />
                 </StackLayout>
-                <Button class="action mt-m" :text="$t('common.back-login')" @tap="doLogout" />
-                <Button class="action validate" :text="$t('common.next-step')" @tap="tabSelected++" />
+                <Label :text="$t('common.required-fields')" />
+                <!-- action buttons -->
+                <FlexboxLayout flexDirection="column" alignItems="center" class="mt-l">
+                  <Button class="action" :text="$t('common.back-login')" @tap="doLogout" />
+                  <Button class="action big validate" :text="$t('common.next-step')" :isEnabled="isStep1Valid()" @tap="tabSelected++" />
+                </flexboxlayout>
               </StackLayout>
             </ScrollView>
           </flexboxlayout>
@@ -31,8 +35,13 @@
                 <!-- diets -->
                 <Label :text="$t('signup.step-2')" class="fz-m mb-l" textWrap="true" />
                 <Pick :data="userDiets" :items="{ diets }" @change="onDietsPickChange" />
-                <Button class="action mt-l" :text="$t('common.previous-step')" @tap="tabSelected--" />
-                <Button class="action validate" :text="$t('common.next-step')" @tap="tabSelected++" />
+                <!-- allergens -->
+                <Pick :data="userAllergens" :items="{ allergens }" @change="onAllergensPickChange" />
+                <!-- action buttons -->
+                <FlexboxLayout flexDirection="column" alignItems="center" class="mt-l">
+                  <Button class="action" :text="$t('common.previous-step')" @tap="tabSelected--" />
+                  <Button class="action big validate" :text="$t('common.next-step')" @tap="tabSelected++" />
+                </flexboxlayout>
               </StackLayout>
             </ScrollView>
           </flexboxlayout>
@@ -43,11 +52,15 @@
           <FlexboxLayout flexDirection="column">
             <ScrollView orientation="vertical" flexGrow="1">
               <StackLayout class="p-l">
-                <!-- allergens -->
+                <!-- restaurant -->
                 <Label :text="$t('signup.step-3')" class="fz-m mb-l" textWrap="true" />
-                <Pick :data="userAllergens" :items="{ allergens }" @change="onAllergensPickChange" />
-                <Button class="action mt-l" :text="$t('common.previous-step')" @tap="tabSelected--" />
-                <Button class="action validate" :text="$t('signup.signup')" :isEnabled="!isLoading" @tap="submit()" />
+                <ListPicker v-model="storeSelected" class="mt-l" :items="stores" @selectedIndexChange="onStoreChange" />
+                <!-- action buttons -->
+                <FlexboxLayout flexDirection="column" alignItems="center" class="mt-l">
+                  <Button class="action" :text="$t('common.previous-step')" @tap="tabSelected--" />
+                  <Button class="action big validate" :text="$t('signup.signup')" :isEnabled="!isLoading" @tap="submit()" />
+                  <ActivityIndicator class="mt-s" :busy="isLoading" />
+                </flexboxlayout>
               </StackLayout>
             </ScrollView>
           </flexboxlayout>
@@ -61,6 +74,7 @@
 import { mapGetters, mapActions } from 'vuex'
 import User from '@/models/User'
 import Pick from '@/components/Pick'
+import Formatter from '@/utils/Formatter'
 
 export default {
   components: {
@@ -83,10 +97,25 @@ export default {
   mounted () {
     this.user = new User({ ...this.storeUser })
     console.log('Signup mounted with user', this.user.email)
+    this.guessNames()
+    this.onStoreChange()
   },
 
   methods: {
     ...mapActions(['goHome', 'doSignup', 'doLogout']),
+    guessNames () {
+      const names = (this.user.email.split('@')[0] || '').split('.')
+      if (!names || !names.length) return
+      if (names[0]) {
+        this.user.firstName = Formatter.capitalizeFirstLetter(names[0])
+      }
+      if (names[1]) {
+        this.user.lastName = Formatter.capitalizeFirstLetter(names[1])
+      }
+    },
+    isStep1Valid () {
+      return (this && this.user.firstName ? this.user.firstName.length : 0) > 3
+    },
     tabChange (args) {
       this.tabSelected = args.value
     },
@@ -97,6 +126,10 @@ export default {
     onAllergensPickChange () {
       this.user.allergens = this.userAllergens.selection.map(s => s.value)
       console.log('onAllergensPickChange', this.user.allergens)
+    },
+    onStoreChange () {
+      this.user.store = this.stores[this.storeSelected]
+      console.log('selected store :', this.user.store)
     },
     submit () {
       console.log('Signup : submit user data :', this.user)
