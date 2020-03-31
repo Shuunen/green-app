84e0e6a48e3908a36745305dc3cb3391af070d47
Romain Racamier-Lafon <romain.racamier@gmail.com>
2020-03-31 18:12:48 +0200

    show user icon in header

--- app/components/header.vue
+++ app/components/header.vue
@@ -1,9 +1,9 @@
 <template>
-  <FlexboxLayout flexDirection="column">
-    <FlexboxLayout class="pt-m pb-m pl-l pr-l bg-primary" justifyContent="space-between" flexShrink="0">
-      <app-icon :name="onHome ? '' : 'home-white'" height="25" @tap.native="$navigateTo(Home)" />
+  <FlexboxLayout flexDirection="column" flexShrink="0">
+    <FlexboxLayout class="pt-m pb-m pl-l pr-l bg-primary" justifyContent="space-between" height="50">
+      <app-icon :name="onHome ? '' : 'home-white'" height="25" width="25" @tap.native="$navigateTo(Home)" />
       <Label class="white fz-m ml-s bold" :text="user.storeLabel" @tap="$navigateTo(Account)" />
-      <app-icon name="settings-white" height="25" @tap.native="$navigateTo(Account)" />
+      <app-icon :name="onAccount ? '' : 'user-white'" height="25" width="25" @tap.native="$navigateTo(Account)" />
     </FlexboxLayout>
   </FlexboxLayout>
 </template>
@@ -15,6 +15,10 @@ import Home from '@/pages/home'
 export default {
   props: {
     onHome: {
+      type: Boolean,
+      default: false,
+    },
+    onAccount: {
       type: Boolean,
       default: false,
     },
