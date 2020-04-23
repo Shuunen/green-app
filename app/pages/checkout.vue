0c0e0ee143f8cef1f5038ac73fcbc963fc40f50a
Romain Racamier-Lafon <romain.racamier@gmail.com>
2020-04-23 15:23:50 +0200

    add checkout page

---
+++ app/pages/checkout.vue
@@ -0,0 +1,42 @@
+<template>
+  <Page actionBarHidden="true">
+    <FlexboxLayout flexDirection="column" class="bg">
+      <ScrollView orientation="vertical" flexGrow="1">
+        <StackLayout>
+          <app-header :user="user" />
+          <app-tile :data="{ type: 'dessert', name: 'Checkout' }" :hero="true" />
+          <StackLayout class="p-m center">
+            <Label text="You need to pay :" class="fz-m mt-l pt-l" />
+            <Label :text="readablePrice(32.40)" class="fz-l mt-m mb-l" />
+            <FlexboxLayout flexDirection="column" alignItems="center" class="mt-l">
+              <Button class="action big validate" :text="$t('order.pay')" @tap="pay()" />
+            </FlexboxLayout>
+          </StackLayout>
+        </StackLayout>
+      </ScrollView>
+    </FlexboxLayout>
+  </Page>
+</template>
+
+<script>
+import { readablePrice } from '@/utils'
+import { apiService } from '@/services'
+
+export default {
+  data () {
+    return {
+      readablePrice,
+      isLoading: false,
+      user: apiService.user,
+    }
+  },
+  created () {
+    console.log('Checkout page created')
+  },
+  methods: {
+    pay () {
+      console.log('user wanna pay')
+    },
+  },
+}
+</script>
