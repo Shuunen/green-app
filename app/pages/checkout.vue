fc39102006134a6705ec17582cc599303e91034b
Romain Racamier-Lafon <romain.racamier@gmail.com>
2020-04-27 11:35:02 +0200

    checkout now dynamic

--- app/pages/checkout.vue
+++ app/pages/checkout.vue
@@ -2,29 +2,35 @@
   <Page actionBarHidden="true">
     <FlexboxLayout flexDirection="column" class="bg">
       <ScrollView orientation="vertical" flexGrow="1">
-        <WebView src="~/pages/stripe.html" />
-        <!--
-        <StackLayout>
-          <app-header :user="user" />
-          <app-tile :data="{ type: 'dessert', name: 'Checkout' }" :hero="true" />
-          <StackLayout class="p-m center">
-            <Label text="You need to pay :" class="fz-m mt-l pt-l" />
-            <Label :text="readablePrice(32.40)" class="fz-l mt-m mb-l" />
-            <FlexboxLayout flexDirection="column" alignItems="center" class="mt-l">
-              <Button class="action big validate" :text="$t('order.pay', { amount : readablePrice(32.40) })" @tap="pay()" />
-            </FlexboxLayout>
-          </StackLayout>
-        </StackLayout>
-        -->
+        <WebView :src="html" />
       </ScrollView>
     </FlexboxLayout>
   </Page>
 </template>
 
 <script>
+import { $t } from '@/utils'
+import pkg from '@/../package.json'
+
 export default {
+  props: {
+    sessionId: {
+      type: String,
+      required: true,
+    },
+  },
+  computed: {
+    html: function () {
+      const hack = '<'
+      return `<!DOCTYPE html><head><script src="https://js.stripe.com/v3/">${hack}/script></head><html><body>
+        <em>${$t('checkout.loading')}</em><script>var stripe = Stripe("${pkg.config.stripe_pub_key}");
+        stripe.redirectToCheckout({ sessionId: "${this.sessionId}" }).then(function (result) {
+          // If redirectToCheckout fails due to a browser or network error, display the localized error message to your customer using result.error.message
+        })${hack}/script></body></html>`
+    },
+  },
   created () {
-    console.log('Checkout page created')
+    console.log('Checkout page created with sessionId', this.sessionId)
   },
 }
 </script>
