f35483ab16d12161be058f2000eb4f1fa4c2a62c
Romain Racamier-Lafon <romain.racamier@gmail.com>
2020-05-05 17:19:02 +0200

    prepare items (products) un-mock

---
+++ app/models/product.js
@@ -0,0 +1,33 @@
+
+export class Product {
+  get title () { return this.name }
+  get value () { return this.internalName }
+
+  constructor ({ id, allergens, name, internalName, family }) {
+    this.id = id || 0
+    this.allergens = allergens || []
+    this.name = name || ''
+    this.internalName = internalName || ''
+    this.family = family || ''
+  }
+}
+
+/* {
+  "@id": "/products/1",
+  "@type": "Product",
+  "productModel": "/product_models/1",
+  "store": "/stores/1",
+  "id": 1,
+  "energy": 0,
+  "fat": 48,
+  "fatLipid": 20,
+  "carbohydrate": 84,
+  "carbohydrateSugar": 3,
+  "proteins": 83,
+  "salt": 10,
+  "allergens": [],
+  "active": true,
+  "name": "Cœur de romaine",
+  "internalName": "base-coeur-de-romaine",
+  "family": "/families/1"
+} */
