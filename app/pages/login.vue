3f10435b30cb17e89e7857779eb776a158671d6f
Romain Racamier-Lafon <romain.racamier@gmail.com>
2020-06-21 12:41:32 +0200

    add fogot password link on login page

--- app/pages/login.vue
+++ app/pages/login.vue
@@ -19,12 +19,14 @@
         <!-- TODO https://github.com/Shuunen/green-app/issues/125 reveal password -->
         <!-- TODO https://github.com/Shuunen/green-app/issues/256 placeholder color -->
         <TextField ref="password" v-model="user.password" :hint="$t('account.password')" secure="true" returnKeyType="done" :isEnabled="!isLoading" />
-        <FlexboxLayout justifyContent="center" class="mt-l">
+
+        <!-- forgot password link -->
+        <Label class="mt-l mb-m link" horizontalAlignment="center" :text="$t('login.forgot-password')" @tap="$navigateTo(ForgotPassword)" />
+
+        <FlexboxLayout flexDirection="column" justifyContent="center" class="mt-l">
           <Button class="action big" :text="hasAccount ? $t('login.login') : $t('login.sign-up')" :class="[hasAccount ? 'validate' : 'validate-alt' ]" :isEnabled="!isLoading" @tap="submit()" />
+          <Button class="action" :text="$t('common.back')" @tap="$navigateTo(PreLogin)" />
         </FlexboxLayout>
-        <Button class="action" :text="$t('common.back-login')" @tap="cancel" />
-        <!-- TODO https://github.com/Shuunen/green-app/issues/224
-        <Label class="mt-s mb-m black" horizontalAlignment="center" :text="$t('login.forgot-password')" />-->
         <ActivityIndicator :busy="isLoading" />
       </StackLayout>
       <app-lang-selector />
@@ -37,6 +39,7 @@ import { apiService } from '@/services'
 import { connectionType, getConnectionType } from 'tns-core-modules/connectivity'
 import Home from '@/pages/home'
 import AccountEdit from '@/pages/account-edit'
+import ForgotPassword from '@/pages/forgot-password'
 import PreLogin from '@/pages/pre-login'
 import { showError, prettyPrint } from '@/utils'
 
@@ -49,24 +52,18 @@ export default {
   },
   data () {
     return {
+      PreLogin,
+      ForgotPassword,
       user: apiService.user,
       isLoading: false,
     }
   },
   mounted () {
     console.log('Login page mounted, user hasAccount ?', this.hasAccount)
-    /*
-    this.user.email = 'user5@example.com'
-    this.user.password = 'user5'
-    */
   },
   methods: {
     focusPassword () {
       this.$refs.password.nativeView.focus()
-    },
-    cancel () {
-      console.log('login : back to pre login')
-      this.$navigateTo(PreLogin)
     },
     async submit () {
       console.log('Login : user submitted data :', this.user.email, this.user.password)
