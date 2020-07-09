b92329c67672487da5c537e7396baa913e3c9a89
b.groleau <groleau.b@gmail.com>
2020-07-09 10:54:05 +0200

    picture in  product Model

--- app/models/product.js
+++ app/models/product.js
@@ -3,13 +3,14 @@ export class Product {
   get title () { return this.name }
   get value () { return this.internalName }
 
-  constructor ({ id, allergens, name, internalName, family, salePrice, price }) {
+  constructor ({ id, allergens, name, internalName, family, salePrice, price, picture }) {
     this.id = id || 0
     this.allergens = allergens || []
     this.name = name || ''
     this.internalName = internalName || ''
     this.family = family || 0
     this.price = salePrice || price || 0
+    this.picture = picture || 'no-image.jpg'
   }
 }
 
